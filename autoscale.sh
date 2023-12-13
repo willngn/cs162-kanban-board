245845102516781eada5d612efaeaf10e52ebb9f
Tam Nguyen <ngttam3010@MacBook-Pro-cua-Tam.local>
2023-12-13 17:08:15 +0800

    Delete kanban_autoscale.yaml file because we can just execute commands in shell file

--- autoscale.sh
+++ autoscale.sh
@@ -4,6 +4,4 @@ minikube addons enable metrics-server
 
 kubectl autoscale deployment kanban-deployment --cpu-percent=50 --min=1 --max=10               
 
-kubectl get hpa
-
 kubectl get hpa kanban-deployment --watch
