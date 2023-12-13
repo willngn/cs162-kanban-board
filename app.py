ac01bedcf4904444f48f314694818098eefe920d
Tam Nguyen <ngttam3010@MacBook-Pro-cua-Tam.local>
2023-12-13 16:31:41 +0800

    Tried different ports for the app and Added enable metrics-server to autoscale.sh

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 from src import app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
--- autoscale.sh
+++ autoscale.sh
@@ -1,5 +1,7 @@
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
+minikube addons enable metrics-server
+
 kubectl autoscale deployment kanban-deployment --cpu-percent=50 --min=1 --max=10               
 
 kubectl get hpa
